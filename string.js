cf69cc9e3375a7758d61ee179b17fdb6a57b87ae
himanshupandey1708 <himanshupandeybhu4@gmail.com>
2025-01-04 19:51:07 +0530

    04/01/2025

---
+++ string.js
@@ -0,0 +1,17 @@
+let str = "Himanshu";
+console.log(str.length);
+
+let txt =  "Hello,World";
+console.log(txt.indexOf("World"));
+
+// string to array conveersion
+
+let text = "Hello, Himanshu Pandey";
+let strArr = Array.from(text);
+console.log(strArr);
+
+strArr.map((curElem,index)=>{
+    console.log(curElem,index);
+})
+
+console.log(text.match("ello"))
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,1 @@
-let fruit = ["apple","abc","banana"];
-
-// fruit.map(curElem,index,fruit) => {
-//     console.log(curElem);
-// }
+text
